1fb2ab5495b98b25cb5968e2c88462b434e0760c
gavinlouuu <gavinlouuu@gmail.com>
2024-04-23 15:29:37 +0800

    crc modbus

--- src/main.cpp
+++ src/main.cpp
@@ -17,16 +17,66 @@
 #include <SPI.h>
 #include <Firebase_ESP_Client.h>
 
-// std::map<String, std::function<void()>> commandMap;
+
+#include "CRC16.h"
+
+CRC16 crc(CRC16_MODBUS_POLYNOME,
+          CRC16_MODBUS_INITIAL,
+          CRC16_MODBUS_XOR_OUT,
+          CRC16_MODBUS_REV_IN,
+          CRC16_MODBUS_REV_OUT);
+
+
+
+void modbus_crc()
+{
+  uint8_t arr[8] = { 0x01, 0x06, 0x00, 0x34, 0x00, 0x01, 0x00, 0x00 }; // Increased size for CRC
+
+  // Add only the relevant data to CRC calculation
+  for (int i = 0; i < 6; i++) { 
+    crc.add(arr[i]);
+  }
+
+  uint16_t crcValue = crc.calc(); // Store the calculated CRC
+
+  // Append CRC in little-endian order
+  arr[6] = crcValue & 0xFF; // Lower byte
+  arr[7] = (crcValue >> 8) & 0xFF; // Higher byte
+
+  Serial.println(crcValue, HEX); // Print the CRC value
+
+  Serial2.write(arr, sizeof(arr)); // Send the array including the CRC
+}
+
+void modbus_off()
+{
+  uint8_t arr[8] = { 0x01, 0x06, 0x00, 0x34, 0x00, 0x00, 0x00, 0x00 }; // Increased size for CRC
+
+  // Add only the relevant data to CRC calculation
+  for (int i = 0; i < 6; i++) { 
+    crc.add(arr[i]);
+  }
+
+  uint16_t crcValue = crc.calc(); // Store the calculated CRC
+
+  // Append CRC in little-endian order
+  arr[6] = crcValue & 0xFF; // Lower byte
+  arr[7] = (crcValue >> 8) & 0xFF; // Higher byte
+
+  Serial.println(crcValue, HEX); // Print the CRC value
+
+  Serial2.write(arr, sizeof(arr)); // Send the array including the CRC
+}
 
 
 int U2_RX = 16;
 int U2_TX = 17;
 
+
 void setup() {
 
   Serial.begin( 115200 ); /* prepare for possible serial debug */
-  Serial2.begin( 38400 );
+  Serial2.begin( 38400, SERIAL_8N1, U2_RX, U2_TX ); /* prepare for possible serial debug */
     // Initialize LittleFS
   if (!LittleFS.begin()) {
     Serial.println("LittleFS mount failed, formatting...");
@@ -50,10 +100,12 @@ void setup() {
   pwmSetup();
   configInit();
   cmdSetup();
-
+  // modbus_crc();
+  modbus_off();
 }
 
 void loop() {
   beagleCLI();
   // put your main code here, to run repeatedly:
 }
+
